4b94b0c6a317b01bba8ce3575ab0883e7b9048f3
minsu.yu <rcow93@hanmail.net>
2019-11-17 00:44:23 +0900

    basic size setting

--- Tetris/main.cpp
+++ Tetris/main.cpp
@@ -4,8 +4,8 @@ int main(int argc, char *argv[])
 {
 	QApplication a(argc, argv);
 
-	//QPointer<MainWindow> mainWnd;
-	//mainWnd = new MainWindow();
+	QPointer<MainWindow> mainWnd;
+	mainWnd = new MainWindow();
 
 	const QString configPath = QString("config.ini");
 	ConfigInstance().loadConfig(configPath);
--- Tetris/mainwindow.cpp
+++ Tetris/mainwindow.cpp
@@ -4,6 +4,15 @@ MainWindow::MainWindow()
 {
 	ui_.setupUi(this);
 
+	setWindowFlags(Qt::Window | Qt::FramelessWindowHint);
+	QDesktopWidget* desktopWidget = QApplication::desktop();
+
+	//int screenCount = desktopWidget->screenCount();
+	QRect rect = desktopWidget->screenGeometry(0);
+
+	this->setFixedSize(rect.width()/2, rect.height()/2);
+	this->move(rect.center() / 2);
+	
 	show();
 }
 
