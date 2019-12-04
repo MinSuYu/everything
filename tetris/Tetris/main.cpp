62d77559a9ec5a9109e68097b27527858bae908b
minsu.yu <rcow93@hanmail.net>
2019-12-04 22:37:42 +0900

    기본 이미지 띄움

--- tetris/Tetris/main.cpp
+++ tetris/Tetris/main.cpp
@@ -4,11 +4,11 @@ int main(int argc, char *argv[])
 {
 	QApplication a(argc, argv);
 
-	QPointer<MainWindow> mainWnd;
-	mainWnd = new MainWindow();
-
 	const QString configPath = QString("config.ini");
 	ConfigInstance().loadConfig(configPath);
+
+	QPointer<MainWindow> mainWnd;
+	mainWnd = new MainWindow();
 
 	return a.exec();
 }
--- tetris/Tetris/mainwindow.cpp
+++ tetris/Tetris/mainwindow.cpp
@@ -7,11 +7,18 @@ MainWindow::MainWindow()
 	setWindowFlags(Qt::Window | Qt::FramelessWindowHint);
 	QDesktopWidget* desktopWidget = QApplication::desktop();
 
-	//int screenCount = desktopWidget->screenCount();
 	QRect rect = desktopWidget->screenGeometry(0);
 
-	this->setFixedSize(rect.width()/2, rect.height()/2);
+	this->setFixedSize(rect.width() / 2, rect.height() / 2);
 	this->move(rect.center() / 2);
+	this->setAttribute(Qt::WA_TranslucentBackground);
+
+	QImage image(QFileInfo(QCoreApplication::applicationDirPath(), "./basicimage.png").absoluteFilePath());
+	QPixmap pixmap = QPixmap::fromImage(image);
+
+	ui_.backgroundLabel->setPixmap(pixmap);
+	ui_.backgroundLabel->move(0, 0);
+	ui_.backgroundLabel->resize(this->width(), this->height());
 	
 	show();
 }
